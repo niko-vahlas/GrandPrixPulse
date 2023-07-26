0567f467cef31dab7f2bd0f977efa36aa33af8e9
UBC Student <arulh@students.cs.ubc.ca>
2023-07-26 12:26:30 -0700

    Update db_tables.sql

--- db_tables.sql
+++ db_tables.sql
@@ -1,30 +1,79 @@
-CREATE TABLE Engine_Manufacturer
-	(engine_model CHAR(50) PRIMARY KEY,
-	company_name CHAR(100));
+CREATE TABLE Engine_Manufacturer (
+	engine_model CHAR(50) PRIMARY KEY,
+	company_name CHAR(100)
+);
 
-CREATE TABLE Car
-	(car_model CHAR(50) PRIMARY KEY);
+CREATE TABLE Car (
+	car_model CHAR(50) PRIMARY KEY
+);
 
-CREATE TABLE Car-Engine
-	(car_model CHAR(50),
+CREATE TABLE Car_Engine (
 	engine_model CHAR(50),
-	PRIMARY KEY (car_model, engine_model),
-	FOREIGN KEY (car_model) REFERENCES Car(car_model),
-	FOREIGN KEY (engine_model) REFERENCES Engine_Manufacturer(engine_model));
+	car_model CHAR(50),
+	PRIMARY KEY (engine_model, car_model),
+	FOREIGN KEY (engine_model) REFERENCES Engine_Manufacturer(engine_model),
+	FOREIGN KEY (car_model) REFERENCES Car(car_model)
+);
 	
-CREATE TABLE Principal
-	(name CHAR(50) PRIMARY KEY);
+CREATE TABLE Principal (
+	name CHAR(50) PRIMARY KEY
+);
 
-CREATE TABLE Driver
-	(name CHAR(50) PRIMARY KEY,
-	driver_number INTEGER);
+CREATE TABLE Driver (
+	name CHAR(50),
+	driver_number INTEGER,
+	points INTEGER,
+	PRIMARY KEY (name)
+);
 
-CREATE TABLE Team
-	(principle_name CHAR(50) NOT NULL,
+CREATE TABLE Team (
+	principle_name CHAR(50) NOT NULL,
 	car_model CHAR(50) NOT NULL,
 	team_name CHAR(50),
-	year INTEGER,
+	year YEAR,
+	season_year YEAR,
 	points INTEGER,
 	PRIMARY KEY (team_name, year),
-	FOREIGN KEY (principle) REFERENCES Principle(name)
-	FOREIGN KEY (car_model) REFERENCES Car(car_model));
+	FOREIGN KEY (principle) REFERENCES Principle(name),
+	FOREIGN KEY (car_model) REFERENCES Car(car_model),
+	FOREIGN KEY (session_yaer) REFERENCES Season(season_year)
+);
+
+CREATE TABLE Team_Driver (
+	driver_name CHAR(50),
+	team_name CHAR(50),
+	year YEAR,
+	PRIMARY KEY (driver_name, team_name, year),
+	FOREIGN KEY (driver_name) REFERENCES Driver(name),
+	FOREIGN KEY (team_name, year) REFERENCES Team(team_name, year)
+);
+
+CREATE TABLE Sponsor (
+	sponsor_name CHAR(50) PRIMARY KEY
+);
+
+CREATE TABLE Team_Sponsor (
+	team_name CHAR(50),
+	year YEAR,
+	sponsor_name,
+	PRIMARY KEY (team_name, year, sponsor_name),
+	FOREIGN KEY (team_name, year) REFERENCES Team(team_name, year),
+	FOREIGN KEY (sponsor_name) REFERENCES Sponsor(sponsor_name)
+);
+
+CREATE TABLE Season (
+	season_year YEAR
+);
+
+CREATE TABLE Race (
+	p1 INTEGER,
+	p2 INTEGER,
+	p3 INTEGER,
+	qp1 INTEGER,
+	qp2 INTEGER,
+	qp3 INTEGER,
+	fastest_lap INTEGER,
+	circuit_name CHAR(50),
+	season_year YEAR,
+	PRIMARY KEY (season_year, circuit_name)
+);
