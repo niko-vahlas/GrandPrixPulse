ae498db0a8d2222a357af7145ceec7bde24a06ad
UBC Student <tzhang48@students.cs.ubc.ca>
2023-07-27 12:38:12 -0700

    Update db_tables.sql

--- db_tables.sql
+++ db_tables.sql
@@ -11,8 +11,8 @@ CREATE TABLE Car_Engine (
 	engine_model CHAR(50),
 	car_model CHAR(50),
 	PRIMARY KEY (engine_model, car_model),
-	FOREIGN KEY (engine_model) REFERENCES Engine_Manufacturer(engine_model) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (car_model) REFERENCES Car(car_model) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (engine_model) REFERENCES Engine_Manufacturer(model) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (car_model) REFERENCES Car(model) ON DELETE CASCADE ON UPDATE CASCADE
 );
 	
 CREATE TABLE Principal (
@@ -35,17 +35,17 @@ CREATE TABLE Team (
 	principal_name CHAR(50) NOT NULL, 
 	PRIMARY KEY (name, year), 
 	FOREIGN KEY (principal) REFERENCES Principal(name) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (car_model) REFERENCES Car(car_model) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (session_year) REFERENCES Season(season_year) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (car_model) REFERENCES Car(model) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (season_year) REFERENCES Season(year) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Team_Driver (
+CREATE TABLE Drives_For (
 	driver_name CHAR(50),
 	team_name CHAR(50),
-	year YEAR,
+	year INTEGER,
 	PRIMARY KEY (driver_name, team_name, year),
 	FOREIGN KEY (driver_name) REFERENCES Driver(name) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (team_name, year) REFERENCES Team(team_name, year) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (team_name, year) REFERENCES Team(name, year) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Season_Champion (
@@ -55,20 +55,20 @@ CREATE TABLE Season_Champion (
 );
 
 CREATE TABLE Sponsor (
-	sponsor_name CHAR(50) PRIMARY KEY
+	name CHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE Team_Sponsor (
+CREATE TABLE Pays_For (
 	team_name CHAR(50),
-	year YEAR,
-	sponsor_name,
+	team_year INTEGER,
+	sponsor_name CHAR(50),
 	PRIMARY KEY (team_name, year, sponsor_name),
-	FOREIGN KEY (team_name, year) REFERENCES Team(team_name, year) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (sponsor_name) REFERENCES Sponsor(sponsor_name) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (team_name, year) REFERENCES Team(name, year) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (sponsor_name) REFERENCES Sponsor(name) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Season (
-	season_year YEAR
+	year INTEGER
 );
 
 CREATE TABLE Race (
@@ -79,17 +79,17 @@ CREATE TABLE Race (
 	qp2 CHAR(50),
 	qp3 CHAR(50),
 	country CHAR(50),
-	fastest_lap INTEGER,
-	circuit_name CHAR(50),
-	season_year YEAR,
-	sc_model CHAR(50),
-	PRIMARY KEY (season_year, circuit_name),
-	FORIEGN KEY (season_year) REFERENCES Season(season_year) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (sc_model) REFERENCES Saftey_Car(model) ON DELETE CASCADE ON UPDATE CASCADE
+	fastest_lap CHAR(8),
+	circuit CHAR(50),
+	year INTEGER,
+	safety_car_model CHAR(50),
+	PRIMARY KEY (year, circuit),
+	FORIEGN KEY (year) REFERENCES Season(year) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (safety_car_model) REFERENCES Saftey_Car(model) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Lap_Record_Set
-	(time CHAR(8) PRIMARY KEY,
+CREATE TABLE Lap_Record_Set (
+	time CHAR(8) PRIMARY KEY,
 	circuit_name CHAR(50),
 	driver_name CHAR(50) NOT NULL,
 	year INTEGER,
