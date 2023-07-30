68007dd62c23f77cf1ee36bf5a900113d55c3b28
Nikolas Vahlas <nikolasvahlas@nikolasvahlas.com>
2023-07-30 12:49:02 -0700

    changed delete.php

--- delete.php
+++ delete.php
@@ -1,14 +1,17 @@
 <?php
 // Connect to your database. Replace dbname, host, username and password with your real info
-$dbh = new PDO('mysql:dbname=f1_database;host=localhost', 'root', 'root');
+$dbh = new PDO('mysql:unix_socket=/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock;dbname=f1_database', 'root', '');
 
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     // Prepare delete statement
-//     $stmt = $dbh->prepare("DELETE FROM Engine_Manufacturer WHERE model = :model");
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Prepare delete statement
+    $stmt = $dbh->prepare("DELETE FROM Engine_Manufacturer WHERE model = :model");
     
-//     // Bind the user input to the statement and execute
-//     $stmt->bindParam(':model', $_POST['engine_model']);
-//     $stmt->execute();
+    // Bind the user input to the statement and execute
+    $stmt->bindParam(':model', $_POST['engine_model']);
+    $stmt->execute();
     
-//     echo "Engine Manufacturer deleted successfully";
+    echo "Engine Manufacturer deleted successfully";
+}
 ?>
