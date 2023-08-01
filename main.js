f739a07f49730e2b3599edcd5433692f43691553
capecod40 <tonyzh1120@gmail.com>
2023-07-31 22:26:02 -0700

    Finished model

--- main.js
+++ main.js
@@ -5,14 +5,9 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.getElementById("render-canvas");
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
 
 const loader = new GLTFLoader();
 
@@ -31,33 +26,59 @@ loader.load( './assets/model/source/mp44.gltf', function ( gltf ) {
 let ambientLight = new THREE.AmbientLight( 0xffffff , .2);
 scene.add( ambientLight );
 
-camera.position.z = 5;
-
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; // 
 
-let directionalLight_top = new THREE.DirectionalLight( 0xffffff, 3 );
-directionalLight_top.castShadow = true;
-directionalLight_top.position.x = 2;
-directionalLight_top.position.y = 5;
-directionalLight_top.position.z = 0;
+let light1 = new THREE.DirectionalLight( 0xffffff, 1.5 );
+light1.castShadow = true;
+light1.position.x = 3;
+light1.position.y = 5;
+light1.position.z = 3;
+light1.shadow.bias = -0.0001;
+light1.shadow.mapSize.width = 256;
+light1.shadow.mapSize.height = 256;
+light1.shadow.camera.near = 0.1;
+light1.shadow.camera.far = 100.0;
+light1.shadow.camera.left = 100;
+light1.shadow.camera.right = -100;
+light1.shadow.camera.top = 100;
+light1.shadow.camera.bottom = -100;
+scene.add(light1);
 
-directionalLight_top.shadow.bias = -0.0001;
-directionalLight_top.shadow.mapSize.width = 256;
-directionalLight_top.shadow.mapSize.height = 256;
-directionalLight_top.shadow.camera.near = 0.1;
-directionalLight_top.shadow.camera.far = 100.0;
-directionalLight_top.shadow.camera.left = 100;
-directionalLight_top.shadow.camera.right = -100;
-directionalLight_top.shadow.camera.top = 100;
-directionalLight_top.shadow.camera.bottom = -100;
-scene.add(directionalLight_top);
+let light2 = new THREE.DirectionalLight( 0xffffff, 1.5 );
+light2.castShadow = true;
+light2.position.x = -3;
+light2.position.y = 5;
+light2.position.z = -3;
+light2.shadow.bias = -0.0001;
+light2.shadow.mapSize.width = 256;
+light2.shadow.mapSize.height = 256;
+light2.shadow.camera.near = 0.1;
+light2.shadow.camera.far = 100.0;
+light2.shadow.camera.left = 100;
+light2.shadow.camera.right = -100;
+light2.shadow.camera.top = 100;
+light2.shadow.camera.bottom = -100;
+scene.add(light2);
+
+
+camera.position.x = -13.651946679824615;
+camera.position.y = 5.006441410947541;
+camera.position.z = -17.5920718905439;
+
+var period = 20;
+var clock = new THREE.Clock();
+var matrix = new THREE.Matrix4();
+
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+	matrix.makeRotationY(clock.getDelta() * 2 * Math.PI / period);
+
+	camera.position.applyMatrix4(matrix);
+
+	camera.lookAt(-0.5017274538153408, 5.006441410947539, 1.4507785714245471);
 
 	renderer.render( scene, camera );
 }
