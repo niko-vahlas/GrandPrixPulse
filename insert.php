701c091c444e7c2711c62d9531639244f1bcfbc2
Nikolas Vahlas <nikolasvahlas@nikolasvahlas.com>
2023-08-07 07:18:20 -0700

    Finished insert

--- insert.php
+++ insert.php
@@ -21,38 +21,23 @@ if ($conn->connect_error) {
 // Retrieve the driver name from the form data
 $driver_name = mysqli_real_escape_string($conn, $_POST['driver_name']);
 
-// Retrieve the teamname from the form data
-$team_name = mysqli_real_escape_string($conn, $_POST['team_name']);
-
 // Retrieve the year from the form data
 $year = mysqli_real_escape_string($conn, $_POST['year']);
 
-// Retrieve the sponsor name from the form data
-$sponsor_name = mysqli_real_escape_string($conn, $_POST['sponsor_name']);
+// Retrieve the lap time from the form data
+$time = mysqli_real_escape_string($conn, $_POST['time']);
 
 // Retrieve the race circuit name from the form data
-$race_circuit_name = mysqli_real_escape_string($conn, $_POST['race_circuit_name']);
-
-// Retrieve the safety car from the form data
-$safety_car = mysqli_real_escape_string($conn, $_POST['safety_car']);
-
-// Retrieve the principal name from the form data
-$principal_name = mysqli_real_escape_string($conn, $_POST['principal_name']);
-
-// Retrieve the car model from the form data
-$car_model = mysqli_real_escape_string($conn, $_POST['car_model']);
-
-// Retrieve the engine model from the form data
-$engine_model = mysqli_real_escape_string($conn, $_POST['engine_model']);
+$race_circuit_name = mysqli_real_escape_string($conn, $_POST['circuit_name']);
 
 
 
-// Redirect to index.html once we are done with deletion
-if ($conn->query($sql) === TRUE) {
-  header("Location: index.html"); 
-  exit;
+$query = "INSERT INTO Lap_Record_Set (time, circuit_name, driver_name, year) VALUES ('$time', '$race_circuit_name', '$driver_name', $year)";
+
+if (mysqli_query($conn, $query)) {
+    echo "New record created successfully";
 } else {
-  echo "Error deleting record: " . $conn->error;
+    echo "Error: " . $query . "<br>" . mysqli_error($conn);
 }
 
 //closes our connection
