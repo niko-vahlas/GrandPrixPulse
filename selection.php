696a43553f22eac1fb37350e400d69fff150043f
capecod40 <tonyzh1120@gmail.com>
2023-08-09 14:18:44 -0700

    Added drop down and checkboxes to selection

--- selection.php
+++ selection.php
@@ -63,28 +63,48 @@
     <div class="container">
       <div class="row">
         <form action="./selection.php" method="post" class="f1-form">
-        <label for="table">Enter table name: </label>
+        <label>Select attributes to show for race car engine manufacturer</label>
+          <label for="model">Model: </label>
           <input
-            type="text"
-            id="table"
-            name="table"
-            class="textbox"
-            required
+            type="checkbox"
+            id="model"
+            name="model"
+            value="checked"
           />
-          <label for="attributes">Enter attribute names separated by commas: </label>
+          <label for="company_name">Company Name: </label>
           <input
-            type="text"
-            id="attributes"
-            name="attributes"
-            class="textbox"
+            type="checkbox"
+            id="company_name"
+            name="company_name"
+            value="checked"
           />
-          <label for="conditions">Enter conditions: </label>
-          <input
-            type="text"
-            id="conditions"
-            name="conditions"
-            class="textbox"
-          />
+          <label for="engine_model">Filter by engine model: </label>
+          <select id="engine_model" name="engine_model" class="textbox">
+          <?php
+
+          // Will report errors if there are any
+          ini_set('display_errors', 1);
+          ini_set('display_startup_errors', 1);
+          error_reporting(E_ALL);
+
+          $servername = "localhost";
+          $username = "root";
+          $password = ""; 
+          $dbname = "f1_database";
+
+          // This creates our connection to the server, we can view the server using the myPHPAdmin link in XAMP
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          $sql = "SELECT model FROM Engine_Manufacturer"; // Adjust the query to match your database schema
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                  echo '<option value="' . $row["model"] . '">' . $row["model"] . '</option>';
+              }
+          } else {
+              echo '<option value="">No engine models found</option>';
+          }
+          ?>
           <input type="submit" value="Submit" class="btn">
         </form>
         <br><br>
@@ -108,38 +128,156 @@
           if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
           }
-          
-          // construct query using input
-          if (!isset($_POST['attributes']) || !isset($_POST['table']) || !isset($_POST['conditions'])) {
-            exit;
-          }
-          $sql = 'SELECT ' . $_POST['attributes'] . ' FROM ' . $_POST['table'] . ' WHERE ' . $_POST['conditions'];
 
-          try {
-            $result = $conn->query($sql);
-          } catch (mysqli_sql_exception $e) {
-            echo "Invalid input: " . $e->getMessage();
-            exit;
-          }
+          if (array_key_exists('engine_model', $_POST) && $_POST['engine_model'] != "") {
+            $empty = true;
+            $sql = 'SELECT ';
+            if (isset($_POST['model']) && $_POST['model'] == 'checked') {
+              $sql = $sql . 'model ';
+              $empty = false;
+              if (isset($_POST['company_name']) && $_POST['company_name'] == 'checked') {
+                $sql = $sql . ', ';
+              }
+            }
 
-          $attribute = $result -> fetch_fields();
+            if (isset($_POST['company_name']) && $_POST['company_name'] == 'checked') {
+              $sql = $sql . 'company_name';
+              $empty = false;
+            }
 
-          echo "<table><tr>";
-          foreach ($attribute as $val) {
-            echo "<td>" . $val -> name . "</td>";
-          }
-          echo "</tr>";
-
-          while($row = mysqli_fetch_row($result)){ 
-              // mysqli_fetch_row fetches one row at a time, loop thru all rows of result
-              echo "<tr>";
-              foreach ($row as $element) {
-                echo "<td>" . $element . "</td>";
+            if (!$empty) {
+              $sql = $sql . " FROM engine_manufacturer WHERE model = '" . $_POST['engine_model'] . "'";
+              $result = $conn->query($sql);
+              $attribute = $result -> fetch_fields();
+    
+              echo "<table><tr>";
+              foreach ($attribute as $val) {
+                echo "<td>" . $val -> name . "</td>";
               }
               echo "</tr>";
+    
+              while($row = mysqli_fetch_row($result)){ 
+                  // mysqli_fetch_row fetches one row at a time, loop thru all rows of result
+                  echo "<tr>";
+                  foreach ($row as $element) {
+                    echo "<td>" . $element . "</td>";
+                  }
+                  echo "</tr>";
+              }
+              echo "</table>";
+            }
           }
-          echo "</table>";
+          //closes our connection
+          $conn->close();
 
+          ?>
+        </div>
+          <br><br>
+
+        <form action="./selection.php" method="post" class="f1-form">
+        <label>Select attributes to show for safety car engine manufacturer</label>
+          <label for="model">Model: </label>
+          <input
+            type="checkbox"
+            id="model"
+            name="model"
+            value="checked"
+          />
+          <label for="manufacturer">Company Name: </label>
+          <input
+            type="checkbox"
+            id="manufacturer"
+            name="manufacturer"
+            value="checked"
+          />
+          <label for="engine_model">Filter by engine model: </label>
+          <select id="engine_model" name="engine_model" class="textbox">
+          <?php
+
+          // Will report errors if there are any
+          ini_set('display_errors', 1);
+          ini_set('display_startup_errors', 1);
+          error_reporting(E_ALL);
+
+          $servername = "localhost";
+          $username = "root";
+          $password = ""; 
+          $dbname = "f1_database";
+
+          // This creates our connection to the server, we can view the server using the myPHPAdmin link in XAMP
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          $sql = "SELECT model FROM safety_car"; // Adjust the query to match your database schema
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                  echo '<option value="' . $row["model"] . '">' . $row["model"] . '</option>';
+              }
+          } else {
+              echo '<option value="">No engine models found</option>';
+          }
+          ?>
+          <input type="submit" value="Submit" class="btn">
+        </form>
+        <div style="color: white">
+          <?php 
+
+          // Will report errors if there are any
+          ini_set('display_errors', 1);
+          ini_set('display_startup_errors', 1);
+          error_reporting(E_ALL);
+
+          $servername = "localhost";
+          $username = "root";
+          $password = ""; 
+          $dbname = "f1_database";
+
+          // This creates our connection to the server, we can view the server using the myPHPAdmin link in XAMP
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          // Checks if we're connected
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+
+          if (array_key_exists('engine_model', $_POST) && $_POST['engine_model'] != "") {
+            $empty = true;
+            $sql = 'SELECT ';
+            if (isset($_POST['model']) && $_POST['model'] == 'checked') {
+              $sql = $sql . 'model ';
+              $empty = false;
+              if (isset($_POST['manufacturer']) && $_POST['manufacturer'] == 'checked') {
+                $sql = $sql . ', ';
+              }
+            }
+
+            if (isset($_POST['manufacturer']) && $_POST['manufacturer'] == 'checked') {
+              $sql = $sql . 'manufacturer';
+              $empty = false;
+            }
+
+            if (!$empty) {
+              $sql = $sql . " FROM safety_car WHERE model = '" . $_POST['engine_model'] . "'";
+              $result = $conn->query($sql);
+              $attribute = $result -> fetch_fields();
+    
+              echo "<table><tr>";
+              foreach ($attribute as $val) {
+                echo "<td>" . $val -> name . "</td>";
+              }
+              echo "</tr>";
+    
+              while($row = mysqli_fetch_row($result)){ 
+                  // mysqli_fetch_row fetches one row at a time, loop thru all rows of result
+                  echo "<tr>";
+                  foreach ($row as $element) {
+                    echo "<td>" . $element . "</td>";
+                  }
+                  echo "</tr>";
+              }
+              echo "</table>";
+            }
+          }
           //closes our connection
           $conn->close();
 
